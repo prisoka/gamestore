935ce18ff4e9726ab2804325132e44fab78b8c3c
Priscilla Okawa <prisoka@gmail.com>
2018-06-19 19:20:41 -0700

    set up routes: get all and get one

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+// << DO NOT FORGET TO REQUIRE KNEX ! ! ! >>
+const knex = require('../knex');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', (req, res, next) => {
+//   res.send('Hi :) here are some users!');
+// });
+
+// route that gets me ALL users:
+router.get('/', (req, res, next) => {
+  // use knex to get all users
+  knex('users')
+    .then((data) => {
+      console.log('data', data)
+      res.send(data)
+    })
+    .catch((err) => {
+      next('SOMETHING WENT WRONG')
+    })
+})
+
+router.get('/:userid', (req, res, next) => {
+  // use knex to get single users
+  knex('users')
+    .where('id', req.params.userid)
+    .then((data) => {
+      console.log('the single user', data)
+      res.send(data)
+    });
 });
+
+
+/*
+// CREATE ONE record for this table
+router.post('/', (req, res, next) => {
+  res.send('CREATED RECORD')
+})
+// UPDATE ONE record for this table
+router.put('/:id', (req, res, next) => {
+  res.send('UPDATED RECORD')
+})
+// DELETE ONE record for this table
+router.delete('/:id', (req, res, next) => {
+  res.send('DELETED RECORD')
+})
+*/
 
 module.exports = router;
