a6c5f0a332e47c89027d2ed4a1e1c563fa2d4441
Priscilla Okawa <prisoka@gmail.com>
2018-06-22 15:25:49 -0700

    added PUT route to update

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ const knex = require('../knex'); /* DO NOT FORGET TO REQUIRE KNEX ! */
 router.get('/', (req, res, next) => {
   // USE KNEX TO GET ALL USERS
   knex('users')
-  .then((data) => {
+  .then( (data) => {
     console.log('data', data)
     res.send(data)
   })
@@ -24,9 +24,9 @@ router.get('/:userid', (req, res, next) => {
   // USE KNEX TO GET A SPECIFIC USER
   knex('users')
   .where('id', req.params.userid)
-  .then((data) => {
-    console.log('the specific user', data)
-    res.send(data)
+  .then((result) => {
+    console.log('the specific user', result)
+    res.send(result)
   })
 })
 
@@ -44,25 +44,56 @@ router.post('/', (req, res, next) => {
   .returning('*')
   /* returning method specifies which column (in this case, all columns) should
   be returned by the insert and update methods */
-  .then((data) => {
+  .then( (data) => {
     let insertedRecord = data[0]
     console.log('data', insertedRecord)
     // conclude the route with res.send():
     res.send(insertedRecord) // we created and now are responding back w/ it
   })
 
-  // what if no name was provided in the body data?
-  // no validation for now
+  // what if no name was provided in the body data? >> no validation for now
 })
 
-// UPDATE ONE record for this table
-// router.put('/:id', (req, res, next) => {
-//   res.send('UPDATED RECORD')
-// })
+// UPDATE ONE record which already exists in db
+router.put('/:userid', (req, res, next) => {
+  console.log('THE PUT ROUTE');
+  // look up a specific user in the database
+  knex('users')
+  .where('id', req.params.userid)
+  .then( (data) => {
+    console.log('the specific user', data)
+
+    // once found, if found, update that user record's data
+    if(data.length) {
+      // user was found, go ahead and update
+      knex('users')
+      .update({
+        name: req.body.name
+      })
+      .where('id', req.params.userid)
+      .returning('*')
+      .then((updateResult) => {
+        console.log('updateResult', updateResult)
+
+        // respond with the user object, represents a record from the user table
+        // conclude the route
+        res.send(updateResult[0])
+      })
+    }
+  })
+})
 
 // DELETE ONE record for this table
-// router.delete('/:id', (req, res, next) => {
-//   res.send('DELETED RECORD')
-// })
+router.delete('/:userid', (req, res, next) => {
+  // look up a specific user (id) in the database
+  // if it exists, delete it
+  knex('users')
+  .where('id', req.params.userid)
+  .del()
+  .then( () => {
+    console.log('')
+  })
+  res.send('DELETED RECORD')
+})
 
 module.exports = router;
