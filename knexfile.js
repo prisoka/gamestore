0add1bf591e4985d89617e62eacf0c0948c65ce8
Priscilla Okawa <prisoka@gmail.com>
2018-06-28 21:43:31 -0700

    setup heroku DB

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://eutwxvengjxsyc:9be4052fc4b463f270e16dbd51afe5caf4c70650b3f2073b3b7c723ef56153c2@ec2-107-22-183-40.compute-1.amazonaws.com:5432/dfnmcm0bt6emo2'
   }
 };
